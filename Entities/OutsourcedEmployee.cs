9c2851a959fb84219fb3ca15b56cfd642288fe60
Petelin <rodrigopf2@hotmail.com>
2023-01-19 13:16:21 -0400

    nova versao

--- Entities/OutsourcedEmployee.cs
+++ Entities/OutsourcedEmployee.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace exercicio_3.Entities
 {
-    class OutsourcedEmployee
+    class OutsourcedEmployee : Employee
     {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee(double additionalCharge) :base(string name, int hours)
+        {
+            AdditionalCharge = additionalCharge;
+        }
     }
+
 }
